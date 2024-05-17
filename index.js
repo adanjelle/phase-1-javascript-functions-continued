edecfa9923c7cbd5123360b21018541a93e77d1a
adanjelle <adanjelle4@gmail.com>
2024-05-17 15:19:43 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun()); 
+
+  function mondayWork(activity ="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  console.log(mondayWork())
+
+  function wrapAdjective(visualFlair = "*") {
+    return function(adjective = "special") {
+      return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("awesome")); // You are !!!awesome!!!!
+
+
